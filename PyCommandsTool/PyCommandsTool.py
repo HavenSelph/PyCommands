f17fbab94dcc2a1189f63b289cd45e701c6618c0
HavenSelph <havenselph@gmail.com>
2022-12-30 13:10:36 -0600

    Rewrote most of the module
    - Added integer passing using "#"
    - Added list passing using either "{}" or "[]"
    - Added keyword passing using "keyword=argument"
    - Added default "help" and "exit" commands
    - Added the ability to bind descriptions to commands
    - Added an execute function that doesn't automatically parse input
    - Added flags to change what the module outputs on errors
    
    - Removed auto-caught errors so the user needs to catch them. This makes the module more consistent in how it would be expected to behave.
    - Changed errors to custom ones so that they can be explicitly caught.

--- PyCommandsTool/PyCommandsTool.py
+++ PyCommandsTool/PyCommandsTool.py
@@ -1,100 +1,245 @@
-"""
-MIT License
-
-Copyright (c) 2020 Haven Selph
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-# ============== Impots ==============
+# Copyright Haven Selph 2022, MIT License, see LICENSE.md file for more info.
 from shlex import split
+import inspect
 
-# ==============  Main  ==============
+
 class Command:
-  def __init__(self, fn, name=None, aliases=None):
-    self.name = name or fn.__name__
-    self.aliases = aliases or []
-    self.fn = fn
+    def __init__(self, funct, names: tuple or list, does):
+        self.funct = funct
+        self.names = names or [funct.__name__]
+        self.args = inspect.getfullargspec(funct)
+        self.does = does
+        self.help_msg = self.usage
 
-  def __call__(self, *args, **kwargs):
-    return self.fn(*args, **kwargs)
+    def run(self, *args, **kwargs):
+        try:
+            return [0, self.funct(args, kwargs)]
+        except Exception as e:
+            return [1, e]
 
-  @property
-  def all_names(self):
-    return self.name, *self.aliases
+    def set_help_msg(self, msg, show_aliases=False, show_usage=False):
+        if show_aliases:
+            self.help_msg = self.usage if show_usage else "" + "\nNames: " + " | ".join(self.names) + msg
+        else:
+            self.help_msg = self.usage if show_usage else "" + msg
+
+    @property
+    def usage(self, name: str = None):
+        if not name:
+            name = self.name
+        params = "|"
+        if not self.params:
+            params = "| this command does not take any parameters"
+        for param in self.params:
+            params += f" <{param}>  "
+
+        return f"| {name}: {self.does}" + "\n" + params
+
+    @property
+    def params(self):
+        return inspect.signature(self.funct).parameters
+
+    @property
+    def name(self):
+        return self.names[0]
 
 
-# ============= Storage =============
 class Commands(dict):
-  def __init__(self, not_found: str='{} was not recognized.', argmismatch: str='{} takes {} arguments but {} were given.'):
-    """
-    Initializes a new command storage object. Check docs for more info.
-    Check docs for all instance variables to interact with module more closely.
+    class CommandExists(TypeError):
+        pass
 
-    not_found: The text to be sent back when a command is not found
-      {0} - The command attempted
+    class NoSuchCommand(KeyError):
+        pass
 
-    argmismatch: The text to be sent back when a command is issue with an invalid amount of arguments.
-      {0} - The command attempted
-      {1} - The amount of arguments required
-      {2} - The amount of arguments passed
-    """
-    self.flag = (not_found, argmismatch)
-    super().__init__()
+    class ParseError(SyntaxError):
+        pass
 
-  def _add_command(self, command):
-    """DO NOT USE UNLESS YOU KNOW HOW. WILL LIKELY BREAK COMMAND."""
-    for cmd_name in command.all_names:
-      if cmd_name in self:
-        raise ValueError(f'Name or alias assigned to function {command.fn.__name__} is duplicate: {cmd_name}')
-      self[cmd_name] = command
-    
-  def add_command(self, name=None, aliases=None):
-    """
-    Addes a new command to the object. Check docs for more info.
+    class NoValidInput(ParseError):
+        pass
 
-    name: The name that the command will use.
-    aliases: The other names the command can be called with.
-    """
-    def inner_fn(fn):
-      self._add_command(Command(fn, name, aliases))
-      return fn
-    return inner_fn
+    class OperatorError(ParseError):
+        pass
 
-  def execute(self, user_input):
-    """
-    Auto parsing function to execute commands. Check docs for more info.
+    class ListError(ParseError):
+        pass
 
-    user_input: A string for the parser to parse.
-    """
-    self.command, *args = self.parse(user_input or 'no_input')
-    if self.command.lower() in self:
-      try:
-        rtrn = self[self.command].fn(*args)
-      except TypeError:
-        return (False, self.flag[1].format(self.command, self[self.command].fn.__code__.co_argcount, len(args)))
-      else:
-        return True, rtrn
-    else:
-      return False, self.flag[0].format(self.command)
-  
-  @staticmethod
-  def parse(string):
-    if (string=='no_input'):
-      return '',''
-    return split(string)
+    class PositionalPlacementError(ParseError):
+        pass
+
+    def __init__(self, *,
+                 no_such_command_flag: str = "{0} is not a command.",
+                 no_valid_input_flag: str = "No valid input was passed.",
+                 operator_error_flag: str = "Value must not be separated with spaces: x=y not x = y",
+                 list_error_flag: str = "Improperly formatted list argument! Should look like: [value1,value2,etc.]",
+                 positional_placement_error_flag: str = "Positional argument follows keyword argument.",
+                 register_help_command=True, register_exit_command=True, register_aliases_command=True):
+        self.commands_no_aliases = []
+        self.NoSuchCommandFlag = no_such_command_flag
+        self.NoValidInputFlag = no_valid_input_flag
+        self.OperatorErrorFlag = operator_error_flag
+        self.ListErrorFlag = list_error_flag
+        self.PositionalPlacementErrorFlag = positional_placement_error_flag
+        if register_help_command:
+            self.__add_command(
+                Command(self.__help_command, ["help", "info"], does="prints out available commands, and their arguments")
+            )
+        if register_aliases_command:
+            self.__add_command(
+                Command(self.__aliases, ["names", "aliases"], does="returns all names for a given command")
+            )
+        if register_exit_command:
+            self.__add_command(
+                Command(exit, ["exit"], does="exits the program using exit()")
+            )
+        super().__init__()
+
+    def __add_command(self, command):
+        for name in command.names:
+            if name in self:
+                raise self.CommandExists(
+                    f"Name or alias assigned to function {command.funct.__name__} is duplicate: {name}"
+                )
+            else:
+                self[name] = command
+        self.commands_no_aliases.append(command.names[0])
+
+    def add_command(self, *names, does=None):
+        def inner_fn(funct):
+            self.__add_command(Command(funct, names, does or "No information provided for this command"))
+            return funct
+
+        return inner_fn
+
+    def __help_command(self, command=None):
+        if command:
+            print(self[command].help_msg)
+        else:
+            print("List of available commands: ")
+            for command in self.commands_no_aliases:
+                print(self[command].usage, end="\n" * 2)
+
+    def __aliases(self, command):
+        try:
+            if self[command]:
+                pass
+        except self.NoSuchCommand:
+            return 1
+        print(f"Registered aliases for {command}")
+        print(", ".join(self[command].names))
+
+    def execute(self, user_input):
+        command, kwargs = self.parse(user_input)
+        if command[0] in self.keys():
+            return 0, self[command[0]].funct(*command[1:], **kwargs), command[0], command[1:], kwargs
+        else:
+            raise self.NoSuchCommand(self.NoSuchCommandFlag.format(command[0]))
+
+    def execute_no_parse(self, cmd: str, *args, **kwargs):
+        if cmd == "":
+            raise self.NoValidInput(self.NoValidInputFlag)
+        if cmd in self.keys():
+            return 0, self[cmd](args, kwargs), cmd, args, kwargs
+
+    def parse(self, value=None):
+        if not value:
+            raise self.NoValidInput(
+                "No valid input was received"
+            )
+        args = []
+        kwargs = {}
+        for argument in split(value):
+            if "=" in argument:
+                if argument == "=" and args:
+                    raise self.OperatorError(
+                        self.OperatorErrorFlag
+                    )
+                a, b = argument.split("=")
+                if b[0] == "#" and b[1:].isnumeric() and args:
+                    kwargs[a] = int(b[1:])
+                elif [b for check in ["[", "(", ")", "]"] if (check in b)] and args:
+                    if (b[0] == "[" and b[-1] == "]") or (b[0] == "(" and b[-1] == ")"):
+                        kwargs[a] = b.replace("[", "").replace("(", "").replace("]", "").replace(")", "").split(",")
+                else:
+                    kwargs[a] = b
+            elif [argument for check in ["[", "(", ")", "]"] if (check in argument)] and args:
+                if (argument[0] == "[" and argument[-1] == "]") or (argument[0] == "(" and argument[-1] == ")"):
+                    args.append(argument.replace("[", "").replace("(", "").replace("]", "").replace(")", "").split(","))
+                else:
+                    raise self.ListError(
+                        self.ListErrorFlag
+                    )
+
+            elif argument[0] == "#" and argument[1:].isnumeric() and kwargs == {} and args:
+                args.append(int(argument[1:]))
+            else:
+                if kwargs == {}:
+                    args.append(argument)
+                else:
+                    raise self.PositionalPlacementError(
+                        self.PositionalPlacementErrorFlag
+                    )
+        if not args:
+            raise self.NoValidInput(
+                self.NoValidInputFlag
+            )
+        return [args, kwargs]
+
+
+if __name__ == "__main__":
+    COMMANDMODULE = Commands()
+
+    # This is a decorator, it passes the below function as the first argument!
+    @COMMANDMODULE.add_command("hi", does="prints hello world")
+    def hi():
+        print("Hello world!")
+
+    # You can pass an infinite number of names for the command to be known as
+    # then at the end, you can pass "does" so you can have a description for it!
+    @COMMANDMODULE.add_command("echo", "repeat", "print", does="prints any passed arguments")
+    def echo(*args):
+        for x in args:
+            print(str(x))
+
+    @COMMANDMODULE.add_command("echotwice", "repeattwice", "printtwice", does="prints any passed arguments; but twice")
+    def echotwice(*args):
+        print(args)
+        for x in args:
+            print(str(x), str(x), sep="\n")
+
+    # Decorators are just a pretty way of doing the below:
+    def add(a: int, b: int, *args: int, print_it: bool=True) -> int:
+        x = sum((a, b, *args))
+        if print_it:
+            print(x)
+        return x
+
+    # Notice how in this, I pass the function as the initial argument. This works EXACTLY the same as the above "decorators"
+    add = COMMANDMODULE.add_command("add", does="returns the sum of all passed arguments (integers required)")(add)
+
+
+    # Now that you've made and registered all your commands, you can create an input loop:
+    try:
+        while True:
+            try:
+                last = COMMANDMODULE.execute(input(">>> "))
+                if last[0] == 0:
+                    # Code did not encounter an error!
+                    pass
+            except COMMANDMODULE.NoValidInput as e:
+                # This is actually a ParseError which can be caught as shown below this except statement!
+                print(e)
+
+            except COMMANDMODULE.ParseError as e:
+                # Code here runs when any parse error is thrown!
+                print(e)
+            except COMMANDMODULE.NoSuchCommand as e:
+                # Code here runs if the command passed doesn't exist!
+                print(e)
+            except TypeError as e:
+                # This is the except statement you need to add.
+                print(e)
+
+    except KeyboardInterrupt:
+        # Code here runs when CTRL+C is pressed, or when KeyboardInterrupt
+        # is thrown as an error.
+        pass
